972e0554a6c513041dba96eb8564294346b63610
Marcel Maqsood <marcel.maqsood@gmail.com>
2019-08-13 19:35:31 +0200

    Funktion für Formularhandler hinzugefügt

--- src/FormularHandlerMiddleware.php
+++ src/FormularHandlerMiddleware.php
@@ -8,10 +8,14 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Zend\Diactoros\Response\JsonResponse;
+use Zend\Json\Json;
 
 
 class FormularHandlerMiddleware implements RequestHandlerInterface
 {
+
+    const STATUS_MISSING_VALUE = 'MISSING_VALUE';
+
     /**
      * @var TemplateRendererInterface
      */
@@ -21,75 +25,60 @@ class FormularHandlerMiddleware implements RequestHandlerInterface
 
     private $transport;
 
-    public function __construct(TemplateRendererInterface $renderer, $formDefinition)
+    private $problem_details;
+
+    public function __construct(TemplateRendererInterface $renderer, $formDefinition, $problem_details)
     {
         $this->renderer = $renderer;
         $this->formDefinition = $formDefinition;
+        $this->problem_details = $problem_details;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $formUtils =  new \Formular\Utils\Utils($this->formDefinition);
-        $submittedData = $formUtils->decodeData($request->getBody()->getContents());
-        $submittedData = $submittedData['data'];
-        if (!isset($submittedData['config'])) {
-            return $formUtils->respond(
-                "submitted formular does not contains a config declaration!",
+        $data = Json::decode($request->getBody()->getContents(), Json::TYPE_ARRAY);
+        //evtl. überprüfen?
+        if(is_null($data) || !is_array($data)){
+            //Api problem shoot
+            return $this->problem_details->createResponse(
+                $this->request,
                 400,
-                self::STATUS_MISSING_VALUE
+                "Data was missing in submitted form.",
+                self::STATUS_MISSING_VALUE,
+                "N/A"
             );
         }
-        $configName = $submittedData['config'];
 
-        $email_informations = $formUtils->getEmailData($configName);
+        $data = $data['data'];
 
-        $transfer_config = $email_informations['transfer_config'];
-        $formDefinition = $email_informations['formDefinition'];
-        $recipients = $email_informations['recipients'];
-        $subject = $email_informations['subject'];
-        $sender = $email_informations['sender'];
-        $senderName = $email_informations['senderName'];
-        $template = $email_informations['template'];
 
-        $templateVariables = ['subject' => $subject];
 
-        foreach ($formDefinition['fields'] as $fieldName => $fieldEntry) {
-            if (isset($fieldEntry['required']) && $fieldEntry['required'] == true) {
-                if (!isset($submittedData[$fieldName])) {
-                    return $formUtils->respond(
-                        "Formular is not valid since "
-                        . $fieldName . " is not defined in submitted data!",
-                        400,
-                        self::STATUS_CONFIG_ERROR
-                    );
-                }
-            }
+    }
 
-            if (isset($submittedData[$fieldName])) {
-                $templateVariables[$fieldName] = $submittedData[$fieldName];
-            }
+    /**
+     * Wandelt Request-Daten in PHP-Array um.
+     *
+     * @param JSON
+     * @return Array
+     */
+    private function decodeData($jsonData){
+
+        try {
+            $data = Json\Json::decode($jsonData, Json\Json::TYPE_ARRAY);
+        } catch (Exception $e) {
+            return $this->respond(
+                "Error decoding request. Expecting valid JSON!",
+                400,
+                self::STATUS_INVALID_REQUEST
+            );
         }
-
-        $this->transport = $formUtils->getTransferer($transfer_config);
-        $renderedMessage = $this->renderer->render(
-            $template,
-            $templateVariables
-        );
-        foreach ($recipients as $recipient) {
-            $mailer = new \Swift_Mailer($this->transport);
-
-            $message = (new \Swift_Message())
-                ->setSubject($subject)
-                ->setFrom([$sender => $senderName])
-                ->setTo([$recipient])
-                ->setBody($renderedMessage);
-            $result = $mailer->send($message);
+        if (!array_key_exists('action', $data) || !array_key_exists('data', $data)) {
+            return $this->respond(
+                "Error decoding request. Invalid format!",
+                400,
+                self::STATUS_INVALID_REQUEST
+            );
         }
-
-        return $formUtils->respond(
-            "E-mail was successful send!",
-            200,
-            $formUtils::STATUS_OK
-        );
+        return $data;
     }
 }
