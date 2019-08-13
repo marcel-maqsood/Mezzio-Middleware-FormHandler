2186fa918682e9f9ea87304667ef2f1993987f4a
Mirko Fenrich <fenrich@designpark.de>
2019-08-13 18:04:47 +0200

    ...

--- src/AbstractAdapter.php
+++ src/AbstractAdapter.php
@@ -4,14 +4,22 @@
 namespace depa\FormularHandlerMiddleware;
 
 
+use Psr\Http\Message\ResponseInterface;
+
 abstract class AbstractAdapter implements AdapterInterface
 {
     public function __construct(array $config, array $requestData)
     {
         //es gibt in der config 2 bereiche. bereich a definiert die formularelemente, bereich b definiert den/die adapter
-        $this->checkConfig($config);
+        $ret = $this->checkConfig($config);
+        if ($ret instanceof ResponseInterface){
+            return $ret;
+        }
         $this->checkRequestData($requestData);
-        $this->handleData();
+        if ($ret instanceof ResponseInterface){
+            return $ret;
+        }
+        return $this->handleData();
     }
 
     protected function checkRequestData($requestData)
@@ -21,5 +29,5 @@ abstract class AbstractAdapter implements AdapterInterface
 
     abstract protected function checkConfig($config);
 
-    abstract public function handleData();
+    abstract public function handleData():ResponseInterface;
 }
