78bf8d5ece8356efc0c017b772f9f50b2657456a
Marcel Maqsood <marcel.maqsood@gmail.com>
2019-08-14 19:07:33 +0200

    Anpassen des Formular-Objekts, hinzufügen von Problem-Details und Funktionen um informationen zu speichern und zu prüfen.

--- src/Formular.php
+++ src/Formular.php
@@ -16,15 +16,55 @@ class Formular
      */
     private $requestData;
 
-    public function __construct(array $config = [], array $requestData = [])
+    private $problemDetails;
+
+    //Es macht keinen Sinn, immer wieder das selbe zu definieren (FormularHandlerMiddleware...), constanten in eigene Klasse auslagern, die man über container abruft?
+    const STATUS_MISSING_VALUE = 'MISSING_VALUE';
+
+    public function __construct(array $config = [], array $requestData = [], $problemDetails)
     {
         $this->config = $config;
         // Aufteilen der Config in Berech mit Formularfaledern und bereich mit  adaptern
         $this->requestData = $requestData;
+
+        $this->problemDetails = $problemDetails;
     }
 
     public function getRequestData() : array
     {
         return $this->requestData;
     }
+
+    public function setRequestData(array $data){
+        $this->requestData = $data;
+    }
+
+    public function getConfig(){
+        return $this->config;
+    }
+
+    public function setConfig(array $data){
+        $this->config = $data;
+    }
+
+    public function checkFormConfigFields(){
+        if(!isset($this->getConfig()['fields']) || is_null($this->getConfig()['fields'] || !is_array($this->getConfig()['fields']))){
+            return $this->problemDetails->createResponse(
+                $this->request,
+                400,
+                "No fields defined in Formular-Config.",
+                self::STATUS_MISSING_VALUE,
+                "N/A"
+            );
+        }
+    }
+
+    public function getFormConfigFields(){
+        return $this->getConfig()['fields'];
+    }
+
+    public function getFormConfigAdapter(){
+        return $this->getConfig()['adapter'];
+    }
+
 }
--- src/FormularFactory.php
+++ src/FormularFactory.php
@@ -5,12 +5,19 @@ declare(strict_types=1);
 namespace depa\FormularHandlerMiddleware;
 
 
+use Zend\ProblemDetails\ProblemDetailsResponseFactory;
+
 class FormularFactory
 {
+
+    private $container;
+
     public function __invoke(ContainerInterface $container) : callable
     {
+        $this->container = $container;
         return function (array $config = [], array $requestData = []) : Formular {
-            return new Formular($config, $requestData);
+            $problemDetails = $this->container->get(ProblemDetailsResponseFactory::class);
+            return new Formular($config, $requestData, $problemDetails);
         };
     }
 }
