b1e65d0220021e066f264f65a16e57fa39ac0583
Marcel Maqsood <marcel.maqsood@gmail.com>
2019-08-19 13:54:46 +0200

    Funktionen hinzugefügt, um Fehler (wie im Formular) abzufangen und auszugeben.

--- src/AbstractAdapter.php
+++ src/AbstractAdapter.php
@@ -8,24 +8,42 @@ use Psr\Http\Message\ResponseInterface;
 
 abstract class AbstractAdapter implements AdapterInterface
 {
+    protected $errorStatus = false;
+
+    protected $errorDescription = '';
+
     protected $formularObj;
+
     protected $config;
 
-    public function __construct(array $requestData, $formularObj)
+    public function __construct(Formular $formularObj)
     {
         $this->formularObj = $formularObj;
         $this->config = $formularObj->getConfig();
 
         //es gibt in der config 2 bereiche. bereich a definiert die formularelemente, bereich b definiert den/die adapter
         $ret = $this->checkConfig($this->config);
-        if ($ret instanceof ResponseInterface){
+        if (is_null($ret)){
             return $ret;
         }
-
-        return $this->handleData();
+        return $this->handleData($formularObj->getValidFields());
     }
 
+    public function setError($errorDescription)
+    {
+        $this->errorStatus = true;
+        $this->errorDescription = $errorDescription;
+    }
 
+    public function getErrorStatus()
+    {
+        return $this->errorStatus;
+    }
+
+    public function getErrorDescription()
+    {
+        return $this->errorDescription;
+    }
 
     abstract protected function checkConfig($config);
 
