9d019cb45989bddb6eb6094b0a0007a6a535bbd4
Marcel Maqsood <marcel.maqsood@gmail.com>
2019-08-20 20:37:26 +0200

    Hinzufügen einer Beschreibung.

--- src/AdapterInterface.php
+++ src/AdapterInterface.php
@@ -6,5 +6,9 @@ namespace depa\FormularHandlerMiddleware;
 
 interface AdapterInterface
 {
+    /**
+     * Soll die Daten basierend auf dem jeweiligen Adapter verarbeiten
+     * @return mixed
+     */
     public function handleData();
 }
