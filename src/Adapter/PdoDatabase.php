8035cd53dddb48721fd40912ffcfa903248ad784
Marcel Maqsood <marcel.maqsood@gmail.com>
2019-09-11 17:50:38 +0200

    Kommentare für PdoDatabase Adapter hinzugefügt und return type von handleData entfernt, da handleData nichts zurück gibt.

--- src/Adapter/PdoDatabase.php
+++ src/Adapter/PdoDatabase.php
@@ -7,17 +7,31 @@ namespace depa\FormularHandlerMiddleware\Adapter;
 use depa\FormularHandlerMiddleware\AbstractAdapter;
 use Psr\Http\Message\ResponseInterface;
 
+/**
+ * Die Klasse speichert die Formulardaten in einer Datenbank.
+ *
+ * Class PdoDatabase
+ * @package depa\FormularHandlerMiddleware\Adapter
+ */
 class PdoDatabase extends AbstractAdapter
 {
 
+    /**
+     * Überprüft das übergebene Config-Array.
+     *
+     * @param $config
+     * @return mixed|void
+     */
     protected function checkConfig($config)
     {
         // TODO: Implement checkConfig() method.
     }
 
-    public function handleData() : ResponseInterface
+    /**
+     *  Speichert die Daten in einer Datenbank
+     */
+    public function handleData()
     {
         // TODO: Implement handleData() method.
-        return null;
     }
 }
